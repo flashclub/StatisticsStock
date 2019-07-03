fb901a2cd15ec071da1e4d9089a80c56484b3672
liangchanglu <liangcl523@163.com>
2019-07-03 18:54:57 +0800

    更新爬虫相关

--- pydata/getdata.py
+++ pydata/getdata.py
@@ -1,15 +1,12 @@
-from pymongo import MongoClient
-client = MongoClient()
-database = client['StatisticsStock']
-collection = database['subscriptioninfos']
-import requests
 import lxml.html
-html =  requests.get('http://www.poems.com.hk/zh-cn/product-and-service/initial-public-offerings/ipo-info/')
-source = html.content.decode()
-selector = lxml.html.fromstring(source)
-item_list = selector.xpath('//table[@class="ipo-scheduled-items"]/tbody/tr/text()')
 
-print(source)
-print(selector)
-print(item_list)
-print('neirong')
+import requests
+
+eurl = 'http://www.poems.com.hk/zh-cn/product-and-service/initial-public-offerings/ipo-info/';
+html_bytes = requests.get(eurl).content
+
+html_str = html_bytes.decode()
+
+selector = lxml.html.fromstring(html_str)
+info = selector.xpath('//*[@id="Scheduled"]/div[3]/table/tbody/tr/td[1]/text()')
+print(info)
