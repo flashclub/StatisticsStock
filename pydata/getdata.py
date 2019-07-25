403ea4508ded396e5985501f8548985cf1557cf1
liangchanglu <liangcl523@163.com>
2019-07-25 18:27:33 +0800

    更新 爬虫

--- pydata/getdata.py
+++ pydata/getdata.py
@@ -56,44 +56,4 @@ if __name__ == "__main__":
 
     print(halfNewData3.return_data())
 
-def getAAStocksData():
-    # getHuiLiData()
-    eurl = 'https://www.aastocks.com/tc/ipo/ListedIPO.aspx'
-    html_bytes = requests.get(eurl)
-    html_content = html_bytes.content
-    html_str = html_content.decode()
-    selector = lxml.html.fromstring(html_str)
-    info = selector.xpath('//*[@class="secRight"]/table/tr[@class="DR"]')
-    list = []
-    names = ['listingDate','code','company','industry','prospectusPrice','marginAmount','handNumberForOne','oneHandSignRate','firstDayGain','nowPrice','totalPrice']
-    for item1 in info:
-        dict1 = {}
-        for index in range(len(item1)):
-            dict1[names[index]] = item1[index].xpath('string(.)').strip()
-        list.append(dict1)
-    return list
-
-def getHuiLiData():
-    eurl = 'http://www.poems.com.hk/zh-cn/product-and-service/initial-public-offerings/ipo-info/';
-    html_bytes = requests.post(eurl)
-    # print(html_bytes)
-    html_content = html_bytes.content
-    # print(html_content)
-    html_str = html_content.decode()
-    # print(html_str)
-    selector = lxml.html.fromstring(html_str)
-    info = selector.xpath('//*[@id="Scheduled"]/div[3]/table/tbody/tr')
-    list = []
-    names = ['code','company','prospectusPrice','listingDate','firstMarginDeadline','publicDeadline','marginAnnualInterest','calculator','marginAmount','sponsor','report','remarks','others',]
-    for item1 in info:
-        dict1 = {}
-        for index in range(len(item1)):
-            dict1[names[index]] = item1[index].xpath('string(.)').strip()
-        list.append(dict1)
-    aas = getAAStocksData()
-    print(aas)
-    print(list)
-    # collection.insert(list)
-    print('添加完毕')
-# getHuiLiData()
 
